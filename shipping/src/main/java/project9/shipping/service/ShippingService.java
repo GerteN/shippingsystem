19d9e6876883a62d8b3b40acbaf677cc21a8f62c
remix96 <remo9611@gmail.com>
2020-12-19 17:00:30 +0100

    Removed some unused imports

--- shipping/src/main/java/project9/shipping/service/ShippingService.java
+++ shipping/src/main/java/project9/shipping/service/ShippingService.java
@@ -2,8 +2,6 @@ package project9.shipping.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
 import project9.shipping.data.ShippingRepository;
 import shipping.Shipping;
 
@@ -12,7 +10,6 @@ public class ShippingService {
 
     @Autowired
     ShippingRepository repository;
-
 
     public Shipping getShipping(Integer UserId){ return repository.findById(UserId).get();}
 }
