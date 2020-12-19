a1a2c3ac6034c22509e1e3179f3aaa6c3c02fdd2
Matteo <matteodilonardo96@gmail.com>
2020-12-19 16:14:59 +0100

    Added dependencies

--- shipping/src/main/java/project9/shipping/data/ShippingRepository.java
+++ shipping/src/main/java/project9/shipping/data/ShippingRepository.java
@@ -1,6 +1,7 @@
 package project9.shipping.data;
 
 import org.springframework.data.repository.CrudRepository;
+import shipping.Shipping;
 
 public interface ShippingRepository extends CrudRepository<Shipping,Integer>{
 }
